0127df1733894683ec379c12e1c0a57e7550b49c
Maxim Sergeev <MaximGrekk@gmail.com>
2022-07-06 00:00:21 +0300

    step: 3-22

--- app.ts
+++ app.ts
@@ -1,42 +1,15 @@
-function logId(id: string | number | boolean) {
-  if (typeof id === "string") {
-    console.log(id);
-  } else if (typeof id === "number") {
-    console.log(id);
-  } else {
-    console.log(id);
-  }
+enum RequestType {
+  GET = "get",
+  POST = "post",
 }
 
-logId(1);
-logId("1");
+// function fetchWithAuth(url: string, method: RequestType) {}
 
-function logError(err: string | string[]) {
-  if (Array.isArray(err)) {
-    console.log(err);
-  } else {
-    console.log(err);
-  }
+function fetchWithAuth(url: string, method: "post" | "get"): 1 | -1 {
+    return 1;
 }
+fetchWithAuth("s", "get");
 
-function logObject(obj: { a: number } | { b: number }) {
-  if ('a' in obj) {
-    console.log(obj.a);
-  } else {
-    console.log(obj.b);
-  }
-}
+let method = 'post';
 
-function logMultipleIds(a: string | number, b: string | boolean) {
-    if (a === b) {
-      console.log(a, b);
-      
-    } else {
-      console.log(a );
-    }
-  }
-
-  let a: 1 = 1;
-  const b = 2;
-
-
+fetchWithAuth('s', method as 'post')
