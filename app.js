10c7a381179924d49c38f34bdbc906c3319cfaf8
Maxim Sergeev <MaximGrekk@gmail.com>
2022-07-19 00:20:19 +0300

    step: 3-25

--- app.js
+++ app.js
@@ -1,40 +1,5 @@
 "use strict";
-function logId(id) {
-    if (typeof id === "string") {
-        console.log(id);
-    }
-    else if (typeof id === "number") {
-        console.log(id);
-    }
-    else {
-        console.log(id);
-    }
-}
-logId(1);
-logId("1");
-function logError(err) {
-    if (Array.isArray(err)) {
-        console.log(err);
-    }
-    else {
-        console.log(err);
-    }
-}
-function logObject(obj) {
-    if ('a' in obj) {
-        console.log(obj.a);
-    }
-    else {
-        console.log(obj.b);
-    }
-}
-function logMultipleIds(a, b) {
-    if (a === b) {
-        console.log(a, b);
-    }
-    else {
-        console.log(a);
-    }
-}
-let a = 1;
-const b = 2;
+const user = {
+    name: 'Maxim',
+    age: 21,
+};
--- app.ts
+++ app.ts
@@ -1,47 +1,18 @@
 interface User {
   name: string;
+}
+
+interface User {
   age: number;
-  skills: string[];
-  log: (id: number) => string;
 }
 
-type User2 =  {
-    name: string;
-    age: number;
-    skills: string[];
-    log: (id: number) => string;
-  }
-
-  
-// interface UserWithRole extends User {
-//   roleId: number;
-// }
-
-interface Role {
-  roleId: number;
+const user: User = {
+    name: 'Maxim',
+    age: 21,
 }
 
-interface UserWithRole extends User, Role {
-  created_at: Date;
+type ID = string | number;
+
+interface IDI {
+    ID: ID
 }
-
-let user: UserWithRole = {
-  name: "Max",
-  age: 33,
-  skills: ["front", "back"],
-  roleId: 1,
-  created_at: new Date(),
-  log(id) {
-    return "";
-  },
-};
-
-interface UserDic {
-    [index: number] : User
-}
-
-type UserDic2 = {
-    [index: number] : User
-}
-
-type UserDic3 = Record<number, User>
