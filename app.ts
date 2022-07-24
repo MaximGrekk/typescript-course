9587775491522c98b8bf8b22129b9cea428b293b
Maxim Sergeev <MaximGrekk@gmail.com>
2022-07-24 22:48:08 +0300

    step: 3-29

--- app.ts
+++ app.ts
@@ -1,28 +1,34 @@
-function logId(id: number | string): void {
-  console.log(id);
+let input: unknown;
+
+input = 3;
+input = ["da", "adad"];
+
+function run(i: unknown) {
+  if (typeof i === "number") {
+    i++;
+  } else {
+    i;
+  }
 }
 
-const a = logId(1);
+run(input);
 
-function multiply(first: number, second?: number) {
-  if (!second) return first * first;
-  return first * second;
+async function getData() {
+  try {
+    fetch("");
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message);
+  }
 }
 
-type voidFunction = () => void;
+async function getDataForce() {
+  try {
+    fetch("");
+  } catch (error) {
+    const e = error as Error;
+  }
+}
 
-const f1: voidFunction = () => {};
+type U1 = unknown | null; // union
 
-const f2: voidFunction = () => {
-  return true;
-};
-
-const b = f2();
-
-const skills = ["devops, js"];
-
-const user = {
-  s: ["s"],
-};
-
-skills.forEach((skill) => user.s.push(skill));
+type I1 = unknown & string; // intersection
